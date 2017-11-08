b90f3787e80834b886a882a4cdb933067f75e8f3
Garrett Hogan <garrett.hogan.dev@gmail.com>
2017-11-07 21:23:39 -0800

    added stage 0 presets for babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       exclude: [/node_modules/],
       use: [{
         loader: 'babel-loader',
-        options: {presets: ['env', 'react']}
+        options: {presets: ['env', 'react', 'stage-0']}
       }]
     }]
   },
